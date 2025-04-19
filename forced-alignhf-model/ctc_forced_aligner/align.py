a2d04c1fea4dc79a5fa841b9e5ccf7d9a2547c35
mendo olame leo <leomendoolame@gmail.com>
2025-04-19 18:45:24 +0100

    clean-projet

--- forced-alignhf-model/ctc_forced_aligner/align.py
+++ forced-alignhf-model/ctc_forced_aligner/align.py
@@ -1,16 +1,16 @@
 import json
-import os
-
+from pydub import AudioSegment
 import torch
+from pathlib import Path
 
-from .alignment_utils import (
+from alignment_utils import (
     generate_emissions,
     get_alignments,
     get_spans,
     load_alignment_model,
     load_audio,
 )
-from .text_utils import postprocess_results, preprocess_text
+from text_utils import postprocess_results, preprocess_text
 
 TORCH_DTYPES = {
     "bfloat16": torch.bfloat16,
@@ -18,115 +18,29 @@ TORCH_DTYPES = {
     "float32": torch.float32,
 }
 
-
 def cli():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--audio_path", help="path of the audio file", required=True)
-    parser.add_argument(
-        "--text_path", help="path of the text to be aligned", required=True
-    )
+    parser.add_argument("--audio_path", help="Path of the audio file", required=True)
+    parser.add_argument("--text_path", help="Path of the text to be aligned", required=True)
+    parser.add_argument("--output_dir", help="Directory to save output segments", required=True)
+    parser.add_argument("--language", type=str, default=None, help="Language in ISO 639-3 code.")
+    parser.add_argument("--romanize", action="store_true", default=False, help="Enable romanization for non-latin scripts.")
+    parser.add_argument("--split_size", type=str, default="word", choices=["sentence", "word", "char"], help="Alignment level.")
+    parser.add_argument("--star_frequency", type=str, default="edges", choices=["segment", "edges"], help="Frequency of <star> token.")
+    parser.add_argument("--merge_threshold", type=float, default=0.00, help="Merge segments closer than this threshold.")
+    parser.add_argument("--alignment_model", default="MahmoudAshraf/mms-300m-1130-forced-aligner", help="CTC model name for alignment.")
+    parser.add_argument("--compute_dtype", type=str, default="float16" if torch.cuda.is_available() else "float32", choices=["bfloat16", "float16", "float32"], help="Compute dtype for alignment inference.")
+    parser.add_argument("--batch_size", type=int, default=4, help="Batch size for inference.")
+    parser.add_argument("--window_size", type=int, default=30, help="Window size in seconds to chunk the audio.")
+    parser.add_argument("--context_size", type=int, default=2, help="Overlap between chunks in seconds.")
+    parser.add_argument("--attn_implementation", type=str, default=None, choices=["eager", "sdpa", "flash_attention_2", None], help="Attention implementation for the model.")
+    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="Device for execution ('cuda' or 'cpu').")
+    parser.add_argument("--segment_audio", action="store_true", help="Enable segmentation of audio based on timestamps.")
+    parser.add_argument("--generate_json", action="store_true", help="Enable generation of JSON file with results.")
+    parser.add_argument("--generate_txt", action="store_true", help="Enable generation of TXT file with results.")
 
-    parser.add_argument(
-        "--language",
-        type=str,
-        default=None,
-        required=True,
-        help="Language in ISO 639-3 code. Identifying the input as Arabic, Belarusian,"
-        " Bulgarian, English, Farsi, German, Ancient Greek, Modern Greek, Pontic Greek"
-        ", Hebrew, Kazakh, Kyrgyz, Latvian, Lithuanian, North Macedonian, Russian, "
-        "Serbian, Turkish, Ukrainian, Uyghur, Mongolian, Thai, Javanese or Yiddish "
-        "will improve romanization for those languages, No effect for other languages.",
-    )
-
-    parser.add_argument(
-        "--romanize",
-        action="store_true",
-        default=False,
-        help="Enable romanization for non-latin scripts. "
-        "Use if you are using a model that doesn't support your language vocabulary.",
-    )
-
-    parser.add_argument(
-        "--split_size",
-        type=str,
-        default="word",
-        choices=["sentence", "word", "char"],
-        help="Whether to align on a sentence, word, or character level.",
-    )
-    parser.add_argument(
-        "--star_frequency",
-        type=str,
-        default="edges",
-        choices=["segment", "edges"],
-        help="The frequency of the <star> token in the text."
-        "Star token increases the accuracy of the alignment but also increases segment"
-        "fragmentation. segment adds <star> token after each segment."
-        "edges adds <star> token at the start and end of the text."
-        "use --merge_threshold to merge segments that are closer than the threshold.",
-    )
-    parser.add_argument(
-        "--merge_threshold",
-        type=float,
-        default=0.00,
-        help="merge segments that are closer than the threshold."
-        "used to remove very small time differences between segments.",
-    )
-
-    parser.add_argument(
-        "--alignment_model",
-        default="MahmoudAshraf/mms-300m-1130-forced-aligner",
-        help="Name of the CTC (Wav2Vec2/HuBERT/MMS) model to use for alignment,"
-        " you can choose a language-specific model or an "
-        "english model along with --romanize flag to support all languages."
-        " accepts Huggingface model names or local pathes.",
-    )
-
-    # compute related arguments
-    parser.add_argument(
-        "--compute_dtype",
-        type=str,
-        default="float16" if torch.cuda.is_available() else "float32",
-        choices=["bfloat16", "float16", "float32"],
-        help="Compute dtype for alignment model inference. Helps with speed and memory usage.",
-    )
-
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=4,
-        help="Batch size for  inference.",
-    )
-
-    parser.add_argument(
-        "--window_size",
-        type=int,
-        default=30,
-        help="ًWindow size in seconds to chunk the audio file for alignment.",
-    )
-
-    parser.add_argument(
-        "--context_size",
-        type=int,
-        default=2,
-        help="ًOverlab between chunks in seconds.",
-    )
-
-    parser.add_argument(
-        "--attn_implementation",
-        type=str,
-        default=None,
-        choices=["eager", "sdpa", "flash_attention_2", None],
-        help="Attention implementation for the model. "
-        "It will chose the fastest implementation by default.",
-    )
-
-    parser.add_argument(
-        "--device",
-        default="cuda" if torch.cuda.is_available() else "cpu",
-        help="if you have a GPU use 'cuda', otherwise 'cpu'",
-    )
     args = parser.parse_args()
 
     model, tokenizer = load_alignment_model(
@@ -141,9 +55,13 @@ def cli():
         model, audio_waveform, args.window_size, args.context_size, args.batch_size
     )
 
-    with open(args.text_path, "r") as f:
-        lines = f.readlines()
-    text = "".join(line for line in lines).replace("\n", " ").strip()
+
+
+    with open(args.text_path, "r", encoding="utf-8") as f:
+        text = f.read().replace("\n", " ").strip()
+
+    # includ bible verse loading after applying load_json function
+    
 
     tokens_starred, text_starred = preprocess_text(
         text, args.romanize, args.language, args.split_size, args.star_frequency
@@ -161,21 +79,40 @@ def cli():
         text_starred, spans, stride, scores, args.merge_threshold
     )
 
-    # write the results to a file
-    with open(f"{os.path.splitext(args.audio_path)[0]}.txt", "w") as f:
-        for result in results:
-            f.write(f"{result['start']}-{result['end']}: {result['text']}\n")
-    # write the results to a json file with the whole text and each segment
-    with open(f"{os.path.splitext(args.audio_path)[0]}.json", "w") as f:
-        json.dump(
-            {
-                "text": text,
-                "segments": results,
-            },
-            f,
-            indent=4,
-        )
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
+    txt_output_path = output_dir / f"{Path(args.audio_path).stem}.txt"
+    json_output_path = output_dir / f"{Path(args.audio_path).stem}.json"
+
+    # Générer le fichier TXT si demandé
+    if args.generate_txt:
+        with open(txt_output_path, "w", encoding="utf-8") as f:
+            for result in results:
+                f.write(f"{result['start']}-{result['end']}: {result['text']}\n")
+
+    # Générer le fichier JSON si demandé
+    if args.generate_json:
+        with open(json_output_path, "w", encoding="utf-8") as f:
+            json.dump({"text": text, "segments": results}, f, indent=4)
+
+    # Segmentation audio si demandé
+    if args.segment_audio:
+        audio = AudioSegment.from_file(args.audio_path, format="wav")
+        for i, result in enumerate(results):
+            start_ms = int(result['start'] * 1000)
+            end_ms = int(result['end'] * 1000)
+
+            # Vérification que les timestamps sont valides
+            if start_ms < end_ms:
+                filename = f"{Path(args.audio_path).stem}_{i+1}"
+                audio_path = (output_dir / filename).with_suffix(".wav")
+                segment = audio[start_ms:end_ms]
+                segment.export(audio_path, format="wav")
+
+                transcript_path = (output_dir / filename).with_suffix(".txt")
+                with open(transcript_path, "w", encoding="utf-8") as f:
+                    f.write(result['text'])
 
 if __name__ == "__main__":
     cli()
